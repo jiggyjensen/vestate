3713e91933ad67b4db8e8b0ab8989f4a323f973c
Jensen Daniel <jadd3791@gmail.com>
2020-03-24 16:56:29 +0100

    structured basic index page

--- app/models/property.rb
+++ app/models/property.rb
@@ -4,4 +4,10 @@ class Property < ApplicationRecord
   validates :user, uniqueness: true
   validates :name, :location, :description, presence: true
   has_one_attached :photo
+  include PgSearch::Model
+  pg_search_scope :search_by_name_and_location,
+  against: [ :name, :location ],
+  using: {
+    tsearch: { prefix: true }
+  }
 end
