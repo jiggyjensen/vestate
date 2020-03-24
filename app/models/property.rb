154f222b4bf916bca889b6a0f297e20bfd1cef1d
Jensen Daniel <jadd3791@gmail.com>
2020-03-24 14:20:03 +0100

    added seedfile and validations to models

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,3 +1,7 @@
 class Property < ApplicationRecord
   belongs_to :user
+  has_many :investments
+  validates :user, uniqueness: true
+  validates :name, :location, :description, presence: true
+  has_one_attached :photo
 end
