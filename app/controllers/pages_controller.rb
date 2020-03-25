adacd1d6d5099c10944e2a18035e44accb496d25
Maxime Geurts <maxime.geurts@hotmail.com>
2020-03-25 18:11:23 +0100

    delete me

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,5 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
-      @properties = Property.all
-      end
-  end
+    @properties = Property.all
 end
