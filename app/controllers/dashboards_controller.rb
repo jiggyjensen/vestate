976ea41ea6fee1d8fe7fc301f835fa5af19a3328
Jensen Daniel <jadd3791@gmail.com>
2020-03-26 16:39:51 +0100

    added solid nav=true to dashboard controller

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -3,5 +3,9 @@ class DashboardsController < ApplicationController
     if current_user.investments.any?
     @investments = current_user.investments
     end
+   end
+
+  def show
+    @solid_nav = true
   end
 end
