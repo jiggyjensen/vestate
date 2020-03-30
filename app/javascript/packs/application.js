f5bcd656b8be08ad4fd7c90bbee3701e615514be
Jensen Daniel <jadd3791@gmail.com>
2020-03-30 12:52:45 +0200

    added datepicker to new property page

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,6 +2,8 @@ import { initNavbarColor} from '../components/navbar';
 import "bootstrap";
 import 'mapbox-gl/dist/mapbox-gl.css';
 import { initMapbox } from '../plugins/init_mapbox';
+import "../plugins/flatpickr"
+
 
 require("chartkick")
 require("chart.js")
