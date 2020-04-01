ab962b65fd979c44297ee5d365c771dda387ab74
Maxime Geurts <maxime.geurts@hotmail.com>
2020-04-01 11:41:11 +0200

    super cool featured prop carouselle

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,7 +5,7 @@ import { initMapbox } from '../plugins/init_mapbox';
 import "../plugins/flatpickr"
 import {mapViewToggle} from '../components/toggle';
 import {sliderCalc} from '../components/slider';
-
+import {featuredProp} from '../components/featured_prop';
 
 require("chartkick")
 require("chart.js")
@@ -14,4 +14,4 @@ initMapbox();
 initNavbarColor();
 mapViewToggle();
 sliderCalc();
-
+featuredProp();
