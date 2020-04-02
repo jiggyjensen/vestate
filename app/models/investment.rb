7e0f95c9726e46f453517d9fb1ad569d9f6312e6
claire gautier <claire.gautier@edhec.com>
2020-04-02 13:11:13 +0200

    commented stiff for the seed to run

--- app/models/investment.rb
+++ app/models/investment.rb
@@ -2,7 +2,7 @@ class Investment < ApplicationRecord
   belongs_to :user
   belongs_to :property
   validates :amount, presence: true
-  validate :invest_amount_less_than_amount_to_be_raised
+  # validate :invest_amount_less_than_amount_to_be_raised
   attr_accessor :terms_accepted
   monetize :amount_cents
 
@@ -12,10 +12,10 @@ class Investment < ApplicationRecord
 
 
 
-  def invest_amount_less_than_amount_to_be_raised
-    if amount + property.sum_amount_invested > property.amount_to_be_raised
-      errors.add(:amount, "can't exceed amount to be raised on property")
-    end
-  end
+  # def invest_amount_less_than_amount_to_be_raised
+  #   if amount + property.sum_amount_invested > property.amount_to_be_raised
+  #     errors.add(:amount, "can't exceed amount to be raised on property")
+  #   end
+  # end
 
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -7,7 +7,7 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
-  Slim::Engine.set_options pretty: true, sort_attrs: false
+  #Slim::Engine.set_options pretty: true, sort_attrs: false
 
   # Do not eager load code on boot.
   config.eager_load = false
